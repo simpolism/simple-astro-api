d13974f61ca778150bd8330177cc074e56460b8d
simpolism <simpolism@gmail.com>
2025-08-29 02:28:44 -0400

    Swap to onbuild

--- plugins/sweph-clone/index.js
+++ plugins/sweph-clone/index.js
@@ -1,4 +1,4 @@
-export const onPostBuild = async function({ utils: { run } }) {
+export const onBuild = async function({ utils: { run } }) {
     await run.command('echo "hello world"');
     await run.command('mv node_modules/sweph/build/ node_modules/sweph/build.bak/');
     await run.command('mv sweph-build node_modules/sweph/build');
