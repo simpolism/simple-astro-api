f451554002eeb052073a9b8bf4d5f5b9712c5a25
simpolism <simpolism@gmail.com>
2025-08-29 02:24:05 -0400

    Split mv command

--- plugins/sweph-clone/index.js
+++ plugins/sweph-clone/index.js
@@ -1,4 +1,5 @@
 export const onPostBuild = async function({ utils: { run } }) {
     await run.command('echo "hello world"');
-    await run.command('mv node_modules/sweph/build node_modules/sweph/build.bak && mv sweph-build node_modules/sweph/build');
+    await run.command('mv node_modules/sweph/build/ node_modules/sweph/build.bak/');
+    await run.command('mv sweph-build node_modules/sweph/build');
 }
